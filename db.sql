012cd9b6c6daad4a8a0056e69be9c8603bc8f455
Emil280 <79112726+Emil280@users.noreply.github.com>
2024-12-07 19:56:55 +0000

    recipe instruction fix
    fix some issues with unsupported characters that caused the instructions to not display correctly in psql

--- db.sql
+++ db.sql
@@ -145,21 +145,21 @@ VALUES (1, 'eggs', 4),
 
 --all recipes are sourced from https://www.jamieoliver.com/
 INSERT INTO recipes (name, instructions, pictureLink, timeToMake) (
-VALUES ('ratatouille', 'Prep your ingredients before you start – peel and cut the onions into wedges, then peel and finely slice the garlic. Trim the aubergines and courgettes, deseed the peppers and chop into 2.5cm chunks. Roughly chop the tomatoes. Pick the basil leaves and set aside, then finely slice the stalks.
+VALUES ('ratatouille', 'Prep your ingredients before you start - peel and cut the onions into wedges, then peel and finely slice the garlic. Trim the aubergines and courgettes, deseed the peppers and chop into 2.5cm chunks. Roughly chop the tomatoes. Pick the basil leaves and set aside, then finely slice the stalks.
 Heat 2 tablespoons of oil in a large casserole pan or saucepan over a medium heat, add the chopped aubergines, courgettes and peppers (you may need to do this in batches) and fry for around 5 minutes, or until golden and softened, but not cooked through. Spoon the cooked veg into a large bowl.
 To the pan, add the onion, garlic, basil stalks and thyme leaves with another drizzle of oil, if needed. Fry for 10 to 15 minutes, or until softened and golden.
 Return the cooked veg to the pan and stir in the fresh and tinned tomatoes, the balsamic and a good pinch of sea salt and black pepper.
 Mix well, breaking up the tomatoes with the back of a spoon. Cover the pan and simmer over a low heat for 30 to 35 minutes, or until reduced, sticky and sweet.
 Tear in the basil leaves, finely grate in the lemon zest and adjust the seasoning, if needed. Serve with a hunk of bread or steamed rice.', 'https://asset.jamieoliver.com/images/cq7w2e71/production/4cf0726436ec7fb974e0afe1a96e9405c9aa4fa6-973x1300.jpg/4cf0726436ec7fb974e0afe1a96e9405c9aa4fa6-973x1300.jpg', '01:15'),
-('crispy chicken', 'Preheat the oven to 200°C. Tear the bread into a food processor, peel and add the garlic, then whiz until fine.
+('crispy chicken', 'Preheat the oven to 200C. Tear the bread into a food processor, peel and add the garlic, then whiz until fine.
 Place the chicken breasts between two sheets of greaseproof paper, then use a rolling pin or the base of a heavy pan to bash and flatten them out to around 1cm thick.
 Lift up the bashed chicken breasts, pour half the breadcrumbs on to the paper, put the chicken back on top, and scatter over the rest of the crumbs. Roughly pat the breadcrumbs on to the chicken, then re-cover with the paper and bash again to help them stick.
-Scrub the potatoes, then slice into wedges (use a crinkle-cut knife, if you’ve got one) and place in a large mixing bowl. Season with a pinch of sea salt and black pepper and drizzle in 1 tablespoon of olive oil, then toss together to coat.
+Scrub the potatoes, then slice into wedges (use a crinkle-cut knife, if you have one) and place in a large mixing bowl. Season with a pinch of sea salt and black pepper and drizzle in 1 tablespoon of olive oil, then toss together to coat.
 Spread out in a single layer on a large baking tray and bake for 35 minutes, or until golden and cooked through, giving the tray a shake and adding the corn to the oven for the last 10 minutes.
 Peel the onion and coarsely grate on a box grater or in a food processor (or slice by hand) with the carrot, apple (discarding the core) and cabbage. Tip into a mixing bowl, mix with the mustard, extra virgin olive oil, vinegar and yoghurt, then season to taste.
 When the wedges have 10 minutes to go, place a large non-stick frying pan on a medium heat with 1 tablespoon of olive oil and fry the chicken for 3 minutes on each side, or until golden and cooked through, adding an extra drizzle of oil, if needed.
 Remove the chicken to a board, slice 1cm thick, and serve with the wedges, corn and slaw. Delicious with tomato ketchup for dipping.', 'https://asset.jamieoliver.com/images/cq7w2e71/production/e02b7f87e1653a32a1e5ee0a543c39b081dad9b9-958x1279.jpg/e02b7f87e1653a32a1e5ee0a543c39b081dad9b9-958x1279.jpg', '01:00'),
-('apple crumble', 'Preheat the oven to 200°C/400°F/gas 6.
+('apple crumble', 'Preheat the oven to 200C/400F/gas 6.
 Peel and core the apples, then quarter and chop into 3cm chunks.
 Place in a saucepan on a medium heat with 100g of sugar and a few fine gratings of lemon zest.
 Pop the lid on and cook for 5 minutes, or until the apples have softened. Remove the heat and leave to cool a little.
@@ -168,12 +168,12 @@ Transfer the apples to a 25cm x 30cm baking dish and sprinkle over the crumble t
 Bake in the oven for 25 to 30 minutes, or until golden and bubbling. Delicious served with vanilla custard.', 'https://asset.jamieoliver.com/images/cq7w2e71/production/89864ff5b1fc78b5eafe15eb5cd9a1749606e6c6-958x1280.jpg/89248401', '00:50'),
 ('avocado ice cream', 'Halve the vanilla pods lengthways, scrape out the seeds, then add to a saucepan with the pods. Add the sugar, and the zest and juice from your lemon and lime.
 Bring to the boil, then simmer for a couple of minutes until all the sugar dissolves.
-Take off the heat, pour into a bowl and leave to cool. Don’t put your finger in for a taste, though, as it could burn you.
+Take off the heat, pour into a bowl and leave to cool. Do not put your finger in for a taste, though, as it could burn you.
 Once the syrup is cool, remove the vanilla pods.
-Peel, destone and scoop the avocado flesh into a liquidiser with the syrup and milk. Blitz to a smooth, light consistency – like a smoothie.
+Peel, destone and scoop the avocado flesh into a liquidiser with the syrup and milk. Blitz to a smooth, light consistency - like a smoothie.
 If you’ve got an ice-cream maker, put your mixture into it and churn till smooth and frozen. If not, pour it into a large baking dish or deep tray and pop it in the freezer, giving it a whisk every half hour or so until frozen and smooth.
 You can eat it right away or transfer it to little containers, cover and pop in the freezer for later.', 'https://asset.jamieoliver.com/images/cq7w2e71/production/208c79314837425aad9f0e173b71c470a878c4e7-958x1280.png/46260596', '01:50'),
-('beetroot, carrot & orange salad', 'Preheat the oven to 200°C/400ºF/gas 6.
+('beetroot, carrot & orange salad', 'Preheat the oven to 200C/400F/gas 6.
 Trim, peel and halve the carrots, then scrub the beets clean, and chop into wedges.
 Parboil the carrots in a large pan of boiling salted water for 5 minutes, then transfer them to a colander using a slotted spoon.
 Carefully lower in the beets and parboil for 5 minutes, then drain (parboiling separately will stop the carrots from turning purple).
@@ -191,7 +191,7 @@ Taste and season, if needed, then ladle the soup into serving bowls or mugs. Sli
 ('eggy bread sandwich', 'Place the slices of bread onto a board and spread over the peanut butter.
 Peel and slice the banana, then arrange over one of the slices, leaving a small gap around the edges.
 Squash over the raspberries, then place the remaining slice of bread on top, peanut butter-side down. Push down lightly to secure.
-Crack the egg into a shallow bowl, add the cinnamon, then whisk with a fork to combine. Dunk the sandwich in the beaten egg, turning to make sure it’s well coated.
+Crack the egg into a shallow bowl, add the cinnamon, then whisk with a fork to combine. Dunk the sandwich in the beaten egg, turning to make sure it is well coated.
 Drizzle 1 teaspoon of oil into a medium non-stick frying pan over a medium-low heat. Once hot, carefully add the eggy sandwich and cook for 3 to 5 minutes, or until golden and crisp, turning every minute.
 Remove to a board and cut into triangles. Serve with the yoghurt, a drizzle of honey and any seasonal fresh fruit you have in your fruit bowl.', 'https://asset.jamieoliver.com/images/cq7w2e71/production/e2ffb07bbcc946363e6a47714b8669f413dea125-958x1280.png/70064535', '00:10')
 );
