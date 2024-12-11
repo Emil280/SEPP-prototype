d0c5eed14b9b2a03fb9f237ce5a0f123a01db12c
Aayush Gautam <53272422+L65FREAD@users.noreply.github.com>
2024-12-11 23:11:12 +0000

    optimize and clean up code

--- Backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+
 import com.example.demo.ApplicationContext.ApplicationContextProvider;
 import com.example.demo.FridgeItem.FridgeItem;
 import com.example.demo.FridgeItem.FridgeItemService;
@@ -24,84 +25,65 @@ import java.util.Optional;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	Request currentRequest;
+
 	@Autowired
-	ApplicationContextProvider applicationContextProvider;
+	private ApplicationContextProvider applicationContextProvider;
+
+	private Request currentRequest;
 
 	public static void main(String[] args) {
-		ApplicationContext myContext = SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	public Optional<Item> getItemFromList(List<Item> itemList, int id){
-		for (Item item : itemList){
-			if (item.getId() == id){
-				return Optional.of(item);
-			}
-		}
-		return Optional.empty();
+	private Optional<Item> getItemFromList(List<Item> itemList, int id) {
+		return itemList.stream().filter(item -> item.getId() == id).findFirst();
 	}
-	public Optional<FridgeItem> getFridgeItemFromList(List<FridgeItem> fridgeItemList, int id){
-		for (FridgeItem fridgeItem : fridgeItemList){
-			if (fridgeItem.getItemId() == id){
-				return Optional.of(fridgeItem);
-			}
-		}
-		return Optional.empty();
+
+	private Optional<FridgeItem> getFridgeItemFromList(List<FridgeItem> fridgeItemList, int id) {
+		return fridgeItemList.stream().filter(fridgeItem -> fridgeItem.getItemId() == id).findFirst();
 	}
+
 	@RequestMapping("/findFilteredRecipes")
-	public String findFilteredRecipes(@RequestBody Request request){
+	public String findFilteredRecipes(@RequestBody Request request) {
 		currentRequest = request;
-		System.out.println(currentRequest.toString());
-		RecipeService myRecipeService = applicationContextProvider.getApplicationContext().getBean(RecipeService.class);
-		RecipeIngredientService myRecipeIngredientService = applicationContextProvider.getApplicationContext().getBean(RecipeIngredientService.class);
-		FridgeItemService myFridgeItemService = applicationContextProvider.getApplicationContext().getBean(FridgeItemService.class);
-		ItemService myItemService = applicationContextProvider.getApplicationContext().getBean(ItemService.class);
-		List<Item> itemList = myItemService.getItems();
-		List<FridgeItem> frigdeItemList = myFridgeItemService.getFridgeItemsByUserId(currentRequest.getUserId());
-		List<Recipe> myValidRecipes = new ArrayList<>();
-		for (Recipe recipe : myRecipeService.getRecipes()){
-			//If the recipe name contains the search
-			boolean found = true;
-			if (recipe.getName().toUpperCase().contains(currentRequest.getSearch().toUpperCase()) && recipe.getTime() <= currentRequest.getTime()){
-				List<RecipeIngredient> ingredientList = myRecipeIngredientService.getRecipeIngredientsById(recipe.getId());
-				for (RecipeIngredient recipeIngredient : ingredientList) {
-					//first if statement checks if the ingredient is in the items table (it should be) if not dont allow recipe
-					//second if statement checks if the recipe adheres to the users request of vegan veg or meat
-					Optional<Item> item = getItemFromList(itemList, recipeIngredient.getItemId());
-					if (item.isPresent()){
-						if (item.get().getType() < currentRequest.getRecipeType()){
-							found = false;
-                            break;
-						}
-					}else {
-						found = false;
-                        break;
-					}
-					//checks if the ingredient is in the users fridge, if not dont allow the recipe
-					Optional<FridgeItem> myFridgeItem = getFridgeItemFromList(frigdeItemList, recipeIngredient.getItemId());
-					if (myFridgeItem.isPresent()) {
-						if (!(myFridgeItem.get().getQuantity() >= recipeIngredient.getQuantity() && myFridgeItem.get().getUserId() == currentRequest.getUserId())) {
-							found = false;
-							break;
-						}
-					} else {
-						found = false;
-						break;
-					}
-				}
-				if (found){
-					myValidRecipes.add(recipe);
-				}
+		RecipeService recipeService = applicationContextProvider.getApplicationContext().getBean(RecipeService.class);
+		RecipeIngredientService recipeIngredientService = applicationContextProvider.getApplicationContext().getBean(RecipeIngredientService.class);
+		FridgeItemService fridgeItemService = applicationContextProvider.getApplicationContext().getBean(FridgeItemService.class);
+		ItemService itemService = applicationContextProvider.getApplicationContext().getBean(ItemService.class);
 
+		List<Item> itemList = itemService.getItems();
+		List<FridgeItem> fridgeItemList = fridgeItemService.getFridgeItemsByUserId(request.getUserId());
+		List<Recipe> validRecipes = new ArrayList<>();
 
+		for (Recipe recipe : recipeService.getRecipes()) {
+			if (isValidRecipe(recipe, itemList, fridgeItemList)) {
+				validRecipes.add(recipe);
 			}
 		}
-		Gson gson = new Gson();
-		String json = gson.toJson(myValidRecipes);
-		System.out.println(json);
-		return json;
 
+		return new Gson().toJson(validRecipes);
 	}
 
+	private boolean isValidRecipe(Recipe recipe, List<Item> itemList, List<FridgeItem> fridgeItemList) {
+		if (!recipe.getName().toUpperCase().contains(currentRequest.getSearch().toUpperCase()) || recipe.getTime() > currentRequest.getTime()) {
+			return false;
+		}
 
+		RecipeIngredientService recipeIngredientService = applicationContextProvider.getApplicationContext().getBean(RecipeIngredientService.class);
+		List<RecipeIngredient> ingredients = recipeIngredientService.getRecipeIngredientsById(recipe.getId());
+
+		for (RecipeIngredient ingredient : ingredients) {
+			Optional<Item> item = getItemFromList(itemList, ingredient.getItemId());
+			if (item.isEmpty() || item.get().getType() < currentRequest.getRecipeType()) {
+				return false;
+			}
+
+			Optional<FridgeItem> fridgeItem = getFridgeItemFromList(fridgeItemList, ingredient.getItemId());
+			if (fridgeItem.isEmpty() || fridgeItem.get().getQuantity() < ingredient.getQuantity() || fridgeItem.get().getUserId() != currentRequest.getUserId()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
