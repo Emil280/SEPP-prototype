48b671fa7270598dfba1f8d25f99d56c1cac63ab
Aayush Gautam <53272422+L65FREAD@users.noreply.github.com>
2024-12-12 00:03:18 +0000

    reverting the find filtered recipies

--- Backend/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -48,24 +48,60 @@ public class DemoApplication {
 	}
 
 	@RequestMapping("/findFilteredRecipes")
-	public String findFilteredRecipes(@RequestBody Request request) {
+	public String findFilteredRecipes(@RequestBody Request request){
+		System.out.println("filtered");
 		currentRequest = request;
-		RecipeService recipeService = applicationContextProvider.getApplicationContext().getBean(RecipeService.class);
-		RecipeIngredientService recipeIngredientService = applicationContextProvider.getApplicationContext().getBean(RecipeIngredientService.class);
-		FridgeItemService fridgeItemService = applicationContextProvider.getApplicationContext().getBean(FridgeItemService.class);
-		ItemService itemService = applicationContextProvider.getApplicationContext().getBean(ItemService.class);
+		RecipeService myRecipeService = applicationContextProvider.getApplicationContext().getBean(RecipeService.class);
+		RecipeIngredientService myRecipeIngredientService = applicationContextProvider.getApplicationContext().getBean(RecipeIngredientService.class);
+		FridgeItemService myFridgeItemService = applicationContextProvider.getApplicationContext().getBean(FridgeItemService.class);
+		ItemService myItemService = applicationContextProvider.getApplicationContext().getBean(ItemService.class);
+		List<Item> itemList = myItemService.getItems();
+		List<FridgeItem> frigdeItemList = myFridgeItemService.getFridgeItemsByUserId(currentRequest.getUserId());
+		List<Recipe> myValidRecipes = new ArrayList<>();
+		for (Recipe recipe : myRecipeService.getRecipes()){
+			//If the recipe name contains the search
+			boolean found = true;
+			if (recipe.getName().toUpperCase().contains(currentRequest.getSearch().toUpperCase()) && recipe.getTime() <= currentRequest.getTime()){
+				List<RecipeIngredient> ingredientList = myRecipeIngredientService.getRecipeIngredientsById(recipe.getId());
+				List <String> ingredientStrings = new ArrayList<>();
+				for (RecipeIngredient recipeIngredient : ingredientList) {
+					//first if statement checks if the ingredient is in the items table (it should be) if not dont allow recipe
+					//second if statement checks if the recipe adheres to the users request of vegan veg or meat
+					Optional<Item> item = getItemFromList(itemList, recipeIngredient.getItemId());
+					if (item.isPresent()){
+						ingredientStrings.add(item.get().getName());
+						if (item.get().getType() < currentRequest.getRecipeType()){
+							found = false;
+							break;
+						}
+					}else {
+						found = false;
+						break;
+					}
+					//checks if the ingredient is in the users fridge, if not dont allow the recipe
+					Optional<FridgeItem> myFridgeItem = getFridgeItemFromList(frigdeItemList, recipeIngredient.getItemId());
+					if (myFridgeItem.isPresent()) {
+						if (!(myFridgeItem.get().getQuantity() >= recipeIngredient.getQuantity() && myFridgeItem.get().getUserId() == currentRequest.getUserId())) {
+							found = false;
+							break;
+						}
+					} else {
+						found = false;
+						break;
+					}
+				}
+				if (found){
+					recipe.setIngredients(ingredientStrings);
+					myValidRecipes.add(recipe);
+				}
 
-		List<Item> itemList = itemService.getItems();
-		List<FridgeItem> fridgeItemList = fridgeItemService.getFridgeItemsByUserId(request.getUserId());
-		List<Recipe> validRecipes = new ArrayList<>();
 
-		for (Recipe recipe : recipeService.getRecipes()) {
-			if (isValidRecipe(recipe, itemList, fridgeItemList)) {
-				validRecipes.add(recipe);
 			}
 		}
+		Gson gson = new Gson();
+		String json = gson.toJson(myValidRecipes);
+		return json;
 
-		return new Gson().toJson(validRecipes);
 	}
 
 	@GetMapping("/randomRecipe")
